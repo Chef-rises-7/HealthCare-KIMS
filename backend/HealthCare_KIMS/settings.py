30cc2ede9f4db6e29aac3da22573477595463278
jpratik15 <190010034@iitdh.ac.in>
2021-10-07 16:01:19 +0530

    Deploy_1

--- backend/HealthCare_KIMS/settings.py
+++ backend/HealthCare_KIMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z!(_^r0ibd(t*d&dt#ug3-3f-s=&(9lsq8p85b^#p14l7t-s_(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '10.196.6.86', '127.0.0.1','0.0.0.0','10.196.8.137','10.196.4.79']
+ALLOWED_HOSTS = ['localhost', '10.196.6.86', '127.0.0.1','0.0.0.0','10.196.8.137','10.196.4.79',"https://healthcarekims.herokuapp.com/ "]
 
 
 # Application definition
